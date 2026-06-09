944bc2dfbf94a411de2cf2ab0bd18f21dc0e8bfd
Bintang <bintangqurne7@gmail.com>
2026-06-10 01:50:15 +0700

    update

--- components/scroll-reveal.tsx
+++ components/scroll-reveal.tsx
@@ -22,7 +22,7 @@ const variants = {
     opacity: 1,
     x: 0,
     y: 0,
-    transition: { duration: 0.6, ease: [0.22, 1, 0.36, 1] },
+    transition: { duration: 0.6, ease: [0.22, 1, 0.36, 1] as [number, number, number, number] },
   },
 };
 
